ce9561e443a4602afa130ae3ad39192231db0f34
Tomasz Misiukiewicz <tomasz.misiukiewicz@callstack.com>
2023-03-28 12:42:15 +0200

    Revert "generate js files before prebuild"
    This reverts commit 7a18e01e3e94776d9bd50481093430d5c976bdcd.

--- plugins/withApiKey.js
+++ plugins/withApiKey.js
@@ -1,16 +1,14 @@
-"use strict";
-exports.__esModule = true;
-var withApiKey = function (config, _a) {
-    var apiKey = _a.apiKey;
+module.exports = function (config, { apiKey }) {
     // Ensure the objects exist
     if (!config.ios) {
-        config.ios = {};
+      config.ios = {};
     }
     if (!config.ios.infoPlist) {
-        config.ios.infoPlist = {};
+      config.ios.infoPlist = {};
     }
+  
     // Append the apiKey
-    config.ios.infoPlist["MY_CUSTOM_NATIVE_IOS_API_KEY"] = apiKey;
+    config.ios.infoPlist['MY_CUSTOM_NATIVE_IOS_API_KEY'] = apiKey;
+  
     return config;
-};
-exports["default"] = withApiKey;
+  };
