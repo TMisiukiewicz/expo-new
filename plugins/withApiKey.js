7a18e01e3e94776d9bd50481093430d5c976bdcd
Tomasz Misiukiewicz <tomasz.misiukiewicz@callstack.com>
2023-03-28 12:41:05 +0200

    generate js files before prebuild

--- plugins/withApiKey.js
+++ plugins/withApiKey.js
@@ -1,14 +1,16 @@
-module.exports = function (config, { apiKey }) {
+"use strict";
+exports.__esModule = true;
+var withApiKey = function (config, _a) {
+    var apiKey = _a.apiKey;
     // Ensure the objects exist
     if (!config.ios) {
-      config.ios = {};
+        config.ios = {};
     }
     if (!config.ios.infoPlist) {
-      config.ios.infoPlist = {};
+        config.ios.infoPlist = {};
     }
-  
     // Append the apiKey
-    config.ios.infoPlist['MY_CUSTOM_NATIVE_IOS_API_KEY'] = apiKey;
-  
+    config.ios.infoPlist["MY_CUSTOM_NATIVE_IOS_API_KEY"] = apiKey;
     return config;
-  };
+};
+exports["default"] = withApiKey;
